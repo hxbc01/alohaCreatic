70cce6af7af51b26c4ed733e8d6ae03b4a86ab60
Irvan Saharudin <irvansaharudin@gmail.com>
2022-10-27 22:13:17 +0700

    joki changes

--- components/Header/navbar.tsx
+++ components/Header/navbar.tsx
@@ -1,41 +1,40 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 import { BsSearch } from "react-icons/bs";
-import { IconContext } from 'react-icons';
+import { IconContext } from "react-icons";
+import styles from "./search.module.css";
 
 export default function Navbar() {
   return (
-    <nav className='mt-8'>
-      <div className='flex justify-between items-center'>
-        <div className='mx-4 font-bold text-md'>
+    <nav className="mt-8">
+      <div className="flex justify-between items-center">
+        <div className="mx-4 font-bold text-md">
           <Link href={"/"} passHref>
             ALOHA
           </Link>
-          <p className='font-normal'>Creatic</p>
+          <p className="font-normal">Creatic</p>
         </div>
         <div>
-          <ul className='flex gap-6'>
-            <li className='cursor-pointer hover:font-semibold '>Home</li>
-            <li className='cursor-pointer hover:font-semibold '>About</li>
-            <li className='cursor-pointer hover:font-semibold '>Services</li>
+          <ul className="flex gap-6">
+            <li className="cursor-pointer hover:font-semibold ">Home</li>
+            <li className="cursor-pointer hover:font-semibold ">About</li>
+            <li className="cursor-pointer hover:font-semibold ">Services</li>
           </ul>
         </div>
-        <div className='flex gap-2'>
-          <div className='flex items-center bg-gray-100 rounded-full w-10'>
-            <IconContext.Provider value={{ className: 'mt-1 mx-2', size: '1.2rem' }}>
-              < BsSearch />
-            </IconContext.Provider>
-            <input 
-            className='hidden focus:ease-in-out focus:delay-75 focus:w-64'
-            type="text" />
+        <div className="flex gap-6">
+          <div className={styles.container}>
+            <input className={styles.input} type="text" />
+            <button className={styles.btn}>
+              <BsSearch />
+            </button>
           </div>
           <Link href={"/loginpage"} passHref>
-            <button className='font-medium mx-4 bg-[#F3C7BD] px-6 py-1 rounded-xl hover:shadow-md hover:bg-[#FDAE9C]'>
+            <button className="font-medium mx-4 bg-[#F3C7BD] px-6 py-1 rounded-xl hover:shadow-md hover:bg-[#FDAE9C]">
               Login
             </button>
           </Link>
         </div>
       </div>
-    </nav >
-  )
+    </nav>
+  );
 }
